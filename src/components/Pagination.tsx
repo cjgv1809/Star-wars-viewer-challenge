192e115960735cd91dfcec7cb32d677aa1c76709
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-09-27 00:24:03 -0300

    add controls for keyboard

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,10 +7,38 @@ const Pagination: React.FC<PaginationProps> = ({
   totalPages,
   onPageChange,
 }) => {
+  const handleScroll = () => {
+    setTimeout(() => window.scrollTo({ top: 0, behavior: "smooth" }), 300);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    onPageChange(newPage);
+    handleScroll();
+  };
+
+  const handleArrowRight = () => {
+    onPageChange(page + 1);
+    handleScroll();
+  };
+
+  const handleArrowLeft = () => {
+    onPageChange(page - 1);
+    handleScroll();
+  };
+
+  const handleOnPageChange = (newPage: number) => {
+    onPageChange(newPage);
+    handleScroll();
+  };
+
   return (
     <div className="pagination">
       {page > 1 && (
-        <button type="button" onClick={() => onPageChange(page - 1)}>
+        <button
+          type="button"
+          onClick={() => handleOnPageChange(page - 1)}
+          onKeyDown={handleArrowLeft}
+        >
           <ChevronLeft size={24} />
         </button>
       )}
@@ -18,8 +46,7 @@ const Pagination: React.FC<PaginationProps> = ({
         {Array.from({ length: totalPages }, (_, index) => (
           <li key={index} className="pagination__item">
             <a
-              href={`#${index + 1}`}
-              onClick={() => onPageChange(index + 1)}
+              onClick={() => handlePageChange(index + 1)}
               className={`pagination__link ${
                 page === index + 1 ? "active" : ""
               }`}
@@ -30,7 +57,11 @@ const Pagination: React.FC<PaginationProps> = ({
         ))}
       </ul>
       {page < totalPages && (
-        <button type="button" onClick={() => onPageChange(page + 1)}>
+        <button
+          type="button"
+          onClick={() => handleOnPageChange(page + 1)}
+          onKeyDown={handleArrowRight}
+        >
           <ChevronRight size={24} />
         </button>
       )}
