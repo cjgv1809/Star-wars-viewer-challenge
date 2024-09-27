76747dd10ae45a4f68506f8c2a7e70ad38d70bbc
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-09-26 22:51:26 -0300

    move function show no results to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,24 @@
+import type { Character } from "@/types";
+
 export const extractIdFromUrl = (url: string): string | null => {
   const matches = url.match(/\/(\d+)\/$/);
   return matches ? matches[1] : null;
+};
+
+export const shouldShowNoResults = (
+  searchTerm: string,
+  isLoading: boolean,
+  characters: Character[],
+  filteredCharacters: Character[]
+): boolean => {
+  const includesCharacter = filteredCharacters.some((character) =>
+    character.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    !includesCharacter &&
+    !isLoading &&
+    characters.length > 0 &&
+    filteredCharacters.length === 0
+  );
 };
