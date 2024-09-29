c2afb00a97e0705ae76e74c15dd84aa7dbd128d4
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-09-29 14:27:30 -0300

    add config for vitest

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import { expect } from "vitest";
+import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+
+declare module "expect" {
+  interface Matchers<R = void>
+    extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+}
